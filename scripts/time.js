3f140c3c67458f104f22aad6b395e7165780e49d
Nubovik <50026919+Nubovik01@users.noreply.github.com>
2020-07-24 19:30:05 +0300

    v1.0
    Other Updates: https://discord.gg/UaUZ5hf

---
+++ scripts/time.js
@@ -0,0 +1,11 @@
+var timeNode2 = document.getElementById('time-node2');
+function getCurrentTimeString2(dots) {
+  var timeString = new Date().toTimeString().replace(/:[0-9]{2,2} .*/, '');
+  return dots ? timeString : timeString.replace(/:/, ' ');
+}
+setInterval(
+  function() { 
+    timeNode2.innerHTML = getCurrentTimeString2(Math.round(Date.now() / 1000) % 2);
+  },
+  1000
+);
