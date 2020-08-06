2c698f4676140dbe4bd73ce1987a2d9d6ff152e4
Ilya <admin@redguy.ru>
2020-08-06 11:30:19 +0300

    Reformat code

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-(function(){
-  'use strict';
+(function () {
+    'use strict';
 
-  class Menu {
-    constructor(settings) {
-      this.nodeMenu = settings.nodeMenu;
-      settings.nodeMenuButton.addEventListener('click', this.toggle.bind(this));
+    class Menu {
+        constructor(settings) {
+            this.nodeMenu = settings.nodeMenu;
+            settings.nodeMenuButton.addEventListener('click', this.toggle.bind(this));
+        }
+
+        toggle() {
+            return this.nodeMenu.classList.toggle('js-menu_activated');
+        }
     }
 
-    toggle() {
-      return this.nodeMenu.classList.toggle('js-menu_activated');
-    }
-  }
+    let nodeMenu = document.querySelector('body');
 
-  let nodeMenu = document.querySelector('body');
-  
-  new Menu({
-    nodeMenu: nodeMenu,
-    nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
-  });
+    new Menu({
+        nodeMenu: nodeMenu,
+        nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
+    });
 })();
